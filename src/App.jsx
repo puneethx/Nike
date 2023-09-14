7abc15e41eb5274ccc9d609ea2b1b2c8dd3c7bae
puneethx <puneethreddyt2004@gmail.com>
2023-09-14 11:12:23 +0530

    Nav added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import Nav from "./components/Nav"
 
 function App() {
   return(
-    <div></div>
+    <main className='relative'>
+      <Nav/>
+    </main>
   )
 }
 
